96500ff6c2bcb25a2d9f8d00c3843b27b9168629
AB1-56-DEVANAZHAGAR <azhagardevan99@gmail.com>
2025-10-03 16:28:58 +0000

    Third commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.png" sizes="any" />
-        <link rel="icon" href="/favicon.png" type="image/png" />
-        <link rel="apple-touch-icon" href="/favicon.png" />
+        <link rel="icon" href="public/favicon.png" sizes="any" />
+        <link rel="icon" href="public/favicon.png" type="image/png" />
+        <link rel="apple-touch-icon" href="public/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&family=Inter:wght@400;500;600&display=swap" rel="stylesheet" />
